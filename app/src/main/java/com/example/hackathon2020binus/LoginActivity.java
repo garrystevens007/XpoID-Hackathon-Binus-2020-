71ee9c0f2c2894292494efb71a7f1021d728ebb3
abhisatria <45398198+abhisatria@users.noreply.github.com>
2020-12-02 02:02:01 +0700

    adding onBackPressed

--- app/src/main/java/com/example/hackathon2020binus/LoginActivity.java
+++ app/src/main/java/com/example/hackathon2020binus/LoginActivity.java
@@ -118,10 +118,15 @@ public class LoginActivity extends AppCompatActivity {
         login_btn_backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                onBackPressed();
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 
     public void gso(){
