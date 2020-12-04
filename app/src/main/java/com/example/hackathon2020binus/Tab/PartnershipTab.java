63678c6c52b66d3bc0ba5f1663a695fa8d39d3f0
abhisatria <45398198+abhisatria@users.noreply.github.com>
2020-12-05 02:51:09 +0700

    Adding partnership Tab

--- app/src/main/java/com/example/hackathon2020binus/Tab/PartnershipTab.java
+++ app/src/main/java/com/example/hackathon2020binus/Tab/PartnershipTab.java
@@ -2,22 +2,39 @@ package com.example.hackathon2020binus.Tab;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.hackathon2020binus.Adapter.ExploreAdapter;
 import com.example.hackathon2020binus.R;
+import com.example.hackathon2020binus.Storage.FirebaseStorage;
+import com.example.hackathon2020binus.model.Umkm;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PartnershipTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class PartnershipTab extends Fragment {
 
-
+    ExploreAdapter exploreAdapter;
+    FirebaseFirestore db;
+    FirebaseAuth firebaseAuth;
+    RecyclerView tabPartnership_rv_savedPartnership;
+    ArrayList<Umkm> listUmkm;
+    int count;
 
     public PartnershipTab() {
         // Required empty public constructor
@@ -34,6 +51,45 @@ public class PartnershipTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_partnership_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_partnership_tab, container, false);
+        init(view);
+        // Inflate the layout for this fragment
+
+        tabPartnership_rv_savedPartnership.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+    private void init(View view){
+        db = FirebaseFirestore.getInstance();
+        tabPartnership_rv_savedPartnership = view.findViewById(R.id.TabPartnership_rv_savedPartnership);
+        listUmkm = new ArrayList<>();
+        count = 0;
+        for(String umkms : FirebaseStorage.historyOpenPartnership){
+            DocumentReference documentReference = db.collection("listUMKM").document(umkms);
+            count++;
+            Log.d("hey",umkms);
+            ListenerRegistration registration = documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                    Umkm umkm = new Umkm();
+                    umkm.setNama(value.getData().get("nama").toString());
+                    Log.d("hey nama",value.getData().get("nama").toString());
+                    umkm.setDeksripsi(value.getData().get("deskripsi").toString());
+                    umkm.setGambar(value.getData().get("gambar").toString());
+                    umkm.setId(value.getData().get("id").toString());
+                    umkm.setOpenToFranchise(Boolean.parseBoolean(value.getData().get("openToFranchise").toString()));
+                    umkm.setOpenToPartnership(Boolean.parseBoolean(value.getData().get("openToPartnership").toString()));
+                    umkm.setOwnerName(value.getData().get("ownerName").toString());
+                    umkm.setPhone(value.getData().get("phone").toString());
+                    umkm.setLatitude(Double.parseDouble(value.getData().get("latitude").toString()));
+                    umkm.setLongitude(Double.parseDouble(value.getData().get("longitude").toString()));
+                    listUmkm.add(umkm);
+                    if(count == FirebaseStorage.historyOpenPartnership.size()){
+                        ExploreAdapter exploreAdapter = new ExploreAdapter(getActivity(),listUmkm);
+                        tabPartnership_rv_savedPartnership.setAdapter(exploreAdapter);
+                    }
+                }
+            });
+        }
     }
 }
